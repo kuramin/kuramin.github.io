64946e76b3c49d881f27526b4490284763f7fd0b
Dmitry <zeropunkt@yandex.ru>
2018-07-23 23:56:10 +0200

    form sending

--- contact.php
+++ contact.php
@@ -1,33 +1,18 @@
-<html>
-<head>
-	<title>Форма заявки с сайта</title>
-</head>
-<body>
-
 <?php
-//проверяем, существуют ли переменные в массиве POST
-if(!isset($_POST['fio']) and !isset($_POST['email'])){
-	?> <form action="contact.php" method="post">
-		<input type="text" name="fio" placeholder="Укажите ФИО" required>
-		<input type="text" name="email" placeholder="Укажите e-mail" required>
-		<input type="submit" value="Отправить">
-	   </form> <?php
+$fio = $_POST['fio'];
+$email = $_POST['email'];
+$fio = htmlspecialchars($fio);
+$email = htmlspecialchars($email);
+$fio = urldecode($fio);
+$email = urldecode($email);
+$fio = trim($fio);
+$email = trim($email);
+//echo $fio;
+//echo "<br>";
+//echo $email;
+if (mail("kuramin.dmitry@gmail.com", "From perpage", "Name:".$fio.". E-mail: ".$email ,"From: example2@mail.ru \r\n")) {
+    echo "сообщение успешно отправлено";
 } else {
-	//показываем форму
-	$fio = $_POST['fio'];
-	$email = $_POST['email'];
-	$fio = htmlspecialchars($fio);
-	$email = htmlspecialchars($email);
-	$fio = urldecode($fio);
-	$email = urldecode($email);
-	$fio = trim($fio);
-	$email = trim($email);
-	if (mail("kuramin.dmitry@gmail.com", "Automail from", "Name:".$fio.". E-mail: ".$email ,"From: example2@mail.ru \r\n")){
-		echo "Сообщение успешно отправлено";
-	} else {
-		echo "При отправке сообщения возникли ошибки";
-	}
+    echo "при отправке сообщения возникли ошибки";
 }
 ?>
-</body>
-</html>
